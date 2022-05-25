e1139a46058304e5f59d1303ad558df30599c92a
Lucpeng <lucp@sylu.edu.cn>
2022-05-25 16:36:53 +0800

    🎉项目集成

---
+++ README.md
@@ -0,0 +1,10 @@
+
+>已支持Flutter 3.x
+
+## 如何运行
+
+1. 搭建Flutter开发环境，可参考：https://docs.flutter.dev/get-started/install
+2. 用Android Studio打开项目
+3. 运行`flutter pub get`获取项目依赖
+4. 打开Android或iOS模拟器
+5. 通过Android Studio 运行按钮运行项目
---
+++ android/app/src/main/kotlin/org/devio/flutter/bili/flutter_bili_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.devio.flutter.bili.flutter_bili_app
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+         override fun onCreate(savedInstanceState: Bundle?) {
+                 super.onCreate(savedInstanceState)
+                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                         val window = activity.window
+                         //设置状态栏为透明色，fix 启动时状态栏会灰色闪一下
+                         window.statusBarColor = Color.TRANSPARENT
+                     }
+             }
+}
